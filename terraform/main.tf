4f98c2fb92d948d9d2da566ea43d8816d7abe01a
maran281 <81796595+maran281@users.noreply.github.com>
2023-11-14 16:32:04 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,55 @@ resource "google_pubsub_subscription" "mysubscription1" {
     topic = google_pubsub_topic.pubsubtopic1.name
 
 }
+  terraform {
+    backend "gcs" {
+      bucket = "terraform_state4_pubsub_poc"   
+      prefix = "terraform_state4_pubsub_poc"
+    }
+  }
+   
+  #This bucket will contain the file, which will be picked by cloud function
+  #resource "google_storage_bucket" "source_bucket_4_cf1" {
+  #  name = "trigger_file_bucket1"
+  #  location = "us-east1"
+  #}
+  
+  /* #This bucket will be delete, we are using it for testing purpose
+  resource "google_storage_bucket" "source_bucket_4_cf2" {
+    name = "trigger_file_bucket10"
+    location = "us-east1"
+    #force_destroy = true
+  } */
+  
+  #This bucket will contain the code file which will be used as cloud function code
+  resource "google_storage_bucket" "tf_storage_bucket" {
+    name = "cfpubsub_poc_code_bucket"
+    location = "us-east1"
+  }
+  
+  resource "google_storage_bucket_object" "tf_bucket_object" {
+    name = "main.zip"
+    bucket = google_storage_bucket.tf_storage_bucket.name
+    source = "../code/main.zip"
+  }
+  
+  resource "google_cloudfunctions_function" "tf_cloud_funct" {
+      name = "my_funct_4_cf_pubsub_poc"
+      runtime = "python310"
+      source_archive_bucket = google_storage_bucket.tf_storage_bucket.name
+      source_archive_object = google_storage_bucket_object.tf_bucket_object.name
+      
+      trigger_http = true
+      timeout = 60
+      entry_point = "hello_world"
+  
+      /* depends_on = [ google_storage_bucket_object.tf_bucket_object ] */
+  }
+  
+  resource "google_cloudfunctions_function_iam_member" "invoker" {
+    cloud_function = google_cloudfunctions_function.tf_cloud_funct.name
+    role = "roles/cloudfunctions.invoker"
+    member = "allUsers"
+  }
+  
+  #comment
