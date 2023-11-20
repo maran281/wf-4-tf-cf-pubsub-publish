a65d921fc8ccee9b95ef59ba2eedc1ae7492938b
mukesh sain <mukeshk.sain786@gmail.com>
2023-11-20 20:20:54 +0100

    commit1

--- code/main.py
+++ code/main.py
@@ -7,28 +7,28 @@ from google.cloud import pubsub_v1
  
 def publish_message(request):
     #get the incoming data from http request
-    print("Debug 1")
+            #print("Debug 1")
     request_json = request.get_json()
-    print("Debug 2")
+            #print("Debug 2")
     print(request_json)
-    print("Debug 9")
+            #print("Debug 9")
     topic_name = request_json.get("topic", "default topic name")
-    print("Debug 3")
-    message_data = request_json.get("message","default message content")
-    print("Debug 4")
+            #print("Debug 3")
+    message_data = request_json.get("message","Manoj with default message")
+            #print("Debug 4")
 
     #Create a pub/sub client
     publisher = pubsub_v1.PublisherClient()
-    print("Debug 5")
+            #print("Debug 5")
 
     #Create a topic name
     topic_path = publisher.topic_path("plated-hash-405319", "pubsubmy_topic1")
-    print("Debug 6")
+            #print("Debug 6")
 
     #publish message to a topic
     future = publisher.publish(topic_path, data=message_data.encode("utf-8"))
-    print("Debug 7")
+            #print("Debug 7")
     message_id = future.result
-    print("Debug 8")
+            #print("Debug 8")
     
     return f"Message {message_id} is published to topic {topic_name}"
